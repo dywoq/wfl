4434831a3a5bef40dcf95b84093aef0fc64cc6c6
dywoq <dywoq.contact@gmail.com>
2026-09-25 19:08:38 +0300

    remove internal package
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- internal/proc/mips/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// https://github.com/dywoq/wlf
-
-// Package mips provides functionality and type definitions to parse
-// and decode MIPS R4000 instructions.
-package mips
